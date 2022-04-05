7b93c2496cd64e42594a8c4ac0fad4a07b65a34a
Marko <marko.saraba@gmail.com>
2022-04-06 01:58:52 +0200

    dodate jos 2 vjezbe

---
+++ ex17/src/index.js
@@ -0,0 +1,15 @@
+   //only change code below this line
+function inverseWhile(){
+
+   // var fiveNumbers ='';
+    var fiveNumbers =5;
+    while (fiveNumbers >0){
+        console.log(fiveNumbers);
+        fiveNumbers--;
+    }
+return fiveNumbers;
+}
+
+   //only change code above this line
+console.log(inverseWhile());
+module.exports = inverseWhile;
